6f1bfc210838335fddda74d7b5213dbec56e4d3e
AlexSmith1988 <3032303Abc>
2018-10-20 11:44:40 +0300

    Generic sum map and flatMap

--- src/main/scala/org/entu/essential/scala/sequencing/computations/genericmodeling/GenericSum.scala
+++ src/main/scala/org/entu/essential/scala/sequencing/computations/genericmodeling/GenericSum.scala
@@ -4,23 +4,35 @@ import org.scalatest.Assertions._
 
 object GenericSum {
   def main(args: Array[String]): Unit = {
-    assert(Left(1).fold(String.valueOf, identity) == "1")
-    assert(Right("Test").value == "Test")
-    val sum: Sum[Int, String] = Right("foo")
+    assert(Failure(1).fold(String.valueOf, identity) == "1")
+    assert(Success("Test").value == "Test")
+    val sum: Sum[Int, String] = Success("foo")
 
-    assert("foo" == sum.fold(String.valueOf , identity))
+    assert("foo" == sum.fold(String.valueOf, identity))
   }
 }
 
 sealed trait Sum[A, B] {
-  def fold[C](left: A => C, right: B => C): C =
+  def fold[C](error: A => C, success: B => C): C =
     this match {
-      case Left(value) => left(value)
-      case Right(value) => right(value)
+      case Failure(value) => error(value)
+      case Success(value) => success(value)
+    }
+
+  def map[C](f: B => C): Sum[A, C] =
+    this match {
+      case Success(value) => Success(f(value))
+      case Failure(value) => Failure(value)
+    }
+
+  def flatMap[C](f: B => Sum[A, C]): Sum[A, C] =
+    this match {
+      case Failure(value) => Failure(value)
+      case Success(value) => f(value)
     }
 }
 
-final case class Left[A, B](value: A) extends Sum[A, B]
+final case class Failure[A, B](value: A) extends Sum[A, B]
 
-final case class Right[A, B](value: B) extends Sum[A, B]
+final case class Success[A, B](value: B) extends Sum[A, B]
 
